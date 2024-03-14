43f1069f31f185dc7d86b42190c4b8ef508bd82a
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-03-14 12:12:29 +0000

    fixup! refactor: tighten up cloudflare detection

--- packages/pg/lib/stream.js
+++ packages/pg/lib/stream.js
@@ -3,12 +3,12 @@
  * @returns {Duplex}
  */
 module.exports.getStream = function getStream(ssl) {
-  if (process.release && process.release.name === 'node') {
-    const net = require('net')
-    return new net.Socket()
-  } else {
+  if (isCloudflareRuntime) {
     const { CloudflareSocket } = require('pg-cloudflare')
     return new CloudflareSocket(ssl)
+  } else {
+    const net = require('net')
+    return new net.Socket()
   }
 }
 
@@ -18,11 +18,20 @@ module.exports.getStream = function getStream(ssl) {
  * @returns {Duplex}
  */
 module.exports.getSecureStream = function getSecureStream(options) {
-  if (process.release && process.release.name === 'node') {
-    var tls = require('tls')
-    return tls.connect(options)
-  } else {
+  if (isCloudflareRuntime) {
     options.socket.startTls(options)
     return options.socket
+  } else {
+    var tls = require('tls')
+    return tls.connect(options)
   }
+}
+
+/**
+ * Are we running in a Cloudflare Worker?
+ * 
+ * @returns true if the code is currently running inside a Cloudflare Worker.
+ */
+function isCloudflareRuntime() {
+  return Boolean(process.release && process.release.name !== 'node' && Response && new Response(null, { cf: { thing: true } }).cf.thing === true);
 }
